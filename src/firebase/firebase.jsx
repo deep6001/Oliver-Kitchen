2afe8aa595d86f0a90f056f58e154e320736d2d0
Deep Patel <pateldeep120404@gmail.com>
2025-06-12 00:25:27 +0530

    setup firebase

---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7wFu2--lzvkaEJKN_5y7y18vu9Mq5QFI",
+  authDomain: "oliverkitchen.firebaseapp.com",
+  projectId: "oliverkitchen",
+  storageBucket: "oliverkitchen.firebasestorage.app",
+  messagingSenderId: "1002234409365",
+  appId: "1:1002234409365:web:e33743fb68f9df1c14869c",
+  measurementId: "G-5DDTHVFCKS"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
